c4c81eb8b9724c4539ffc21c8ef398db0e0138b2
DjV8 <bartekkreski@gmail.com>
2021-03-08 20:22:31 +0100

    Don't know how but ifxed

--- bot.js
+++ bot.js
@@ -3,19 +3,51 @@ dotenv.config();
 
 import { Client } from "discord.js";
 import ytdl from "ytdl-core";
-const CLIENT = new Client();
-const QUEUE = new Map();
+const client = new Client();
+const queue = new Map();
 import { readFileSync } from "fs";
 import { createInterface } from "readline";
 import pkg from "winston"; // logger
-const { format: _FORMAT, createLogger, transports: _TRANSPORTS } = pkg;
+const { format: _FORMAT, createLogger, transports: _TRANSPORTS } = pkg; // wczytanie stacji
+
+client.on("ready", () => {
+	let botStatus = {
+		id: 0,
+		list: [
+			"Zawołaj pomocy jak potrzebujesz 😉",
+			`Jesem na ${client.guilds.cache.size} serwerach!`,
+			"Ram pam pam",
+			"🎶🎶🎶",
+		],
+	};
+
+	setInterval(
+		async () => (botStatus.list[1] = `Jesem na ${client.guilds.cache.size} serwerach!`),
+		864e5
+	); //co 24h
+
+	setInterval(async () => {
+		client.user.setPresence({
+			// prezencja https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+			activity: {
+				name: botStatus.list[botStatus.id],
+				type: "PLAYING",
+			},
+			status: "online",
+		});
+		botStatus.id++;
+		if (botStatus.id == botStatus.list.length) botStatus.id = 0;
+	}, 6e5); // co 10 min
+
+	logger.info("Zalogowano jako " + client.user.tag + "!");
+	logger.info("Link z zaproszeniem: " + process.env.BOT_INVITE);
+	rl.question("Wcisnij enter aby zakonczyc\n", () => {
+		client.destroy();
+		process.exit();
+	});
+});
 
 let radiostation = JSON.parse(readFileSync("stations.json", "utf8")); // wczytanie stacji
-
-const CONSOLE = createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
 
 const LOGFORMAT = _FORMAT.printf(({ level, timestamp, message }) => {
 	return `${level}:${timestamp}: ${message}`;
@@ -32,44 +64,13 @@ const logger = createLogger({
 	exceptionHandlers: [new _TRANSPORTS.File({ filename: "exceptions.log" })],
 });
 
-CLIENT.on("ready", () => {
-	let botStatus = {
-		id: 0,
-		list: [
-			"Zawołaj pomocy jak potrzebujesz 😉",
-			`Jesem na ${CLIENT.guilds.cache.size} serwerach!`,
-			"Ram pam pam",
-			"🎶🎶🎶",
-		],
-	};
-
-	setInterval(
-		async () => (botStatus.list[1] = `Jesem na ${CLIENT.guilds.cache.size} serwerach!`),
-		864e5
-	); //co 24h
-
-	setInterval(async () => {
-		CLIENT.user.setPresence({
-			// prezencja https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-			activity: {
-				name: botStatus.list[botStatus.id],
-				type: "PLAYING",
-			},
-			status: "online",
-		});
-		botStatus.id++;
-		if (botStatus.id == botStatus.list.length) botStatus.id = 0;
-	}, 6e5); // co 10 min
-
-	logger.info(`Zalogowano jako ${CLIENT.user.tag}!`);
-	logger.info(`Link z zaproszeniem: ${process.env.BOT_INVITE}`);
-	CONSOLE.question("Wcisnij enter aby zakonczyc\n", () => {
-		CLIENT.destroy();
-		process.exit();
-	});
+const rl = createInterface({
+	// interfejs konsoli
+	input: process.stdin,
+	output: process.stdout,
 });
 
-CLIENT.on("message", async (message) => {
+client.on("message", async (message) => {
 	if (message.author.bot) return;
 
 	if (!message.channel.permissionsFor(message.client.user).has("SEND_MESSAGES"))
@@ -79,112 +80,62 @@ CLIENT.on("message", async (message) => {
 		return message.channel.send("zapierdala");
 
 	if (
-		!message.content.startsWith("<@" + CLIENT.user + ">") &&
-		!message.content.startsWith("<@!" + CLIENT.user + ">")
+		!message.content.startsWith("<@" + client.user + ">") &&
+		!message.content.startsWith("<@!" + client.user + ">")
 	)
 		return;
+
 	const ARGS = message.content.replace(/\s+/g, " ").split(" ");
+	const SERVERQUEUE = queue.get(message.guild.id);
 	if (!ARGS[1]) return message.channel.send("czego kurwa");
-	const SERVERQUEUE = QUEUE.get(message.guild.id);
-	if (ARGS[1] == "odśwież") {
-		const ADMINID = process.env.ADMIN.split(",");
-		for (let i = 0; i < ADMINID.length; i++)
-			if (message.author.id === ADMINID[i]) return refresh(message.channel);
+
+	switch (ARGS[1]) {
+		case "odpal":
+			if (checkIfOnVC(message.member.voice.channel, message.channel, SERVERQUEUE))
+				execute(message, SERVERQUEUE, ARGS[2]);
+			break;
+		case "idź":
+			if (checkIfOnVC(message.member.voice.channel, message.channel, SERVERQUEUE)) {
+				message.channel.send("okok");
+				stop_radio(SERVERQUEUE);
+			}
+			break;
+		case "stacje":
+			list_stations(message.channel);
+			break;
+		case "odśwież":
+			const ADMINID = process.env.ADMIN.split(",");
+			for (let i = 0; i < ADMINID.length; i++)
+				if (message.author.id === ADMINID[i]) refresh(message.channel);
+			break;
+		case "pomocy":
+			message.channel.send(
+				"```Dostępne polecenia:\n@Radio odpal < nazwa stacji > / <link z yt> - dołącza do kanału i odtwarza wybrane radio/film z linka\n@Radip queue - pokazuję aktualną kolejke \n@Radio idź - przestaje grać i wychodzi z kanału\n@Radio stacje - wyświetla dostępne stacje\n@Radio loop - zapętlanie utworów\n@Radio kloop - zapętla całą kolejkę \n@Radio pomiń - pomija element z kolejki```"
+			);
+			break;
+		case "pomiń":
+			if (
+				checkIfOnVC(message.member.voice.channel, message.channel, SERVERQUEUE) &&
+				checkIfQueue(SERVERQUEUE, message.channel)
+			)
+				skip(SERVERQUEUE);
+			break;
+		case "loop":
+		case "kloop":
+			if (
+				checkIfOnVC(message.member.voice.channel, message.channel, SERVERQUEUE) &&
+				checkIfQueue(SERVERQUEUE, message.channel)
+			)
+				loop(message, SERVERQUEUE, ARGS[1]);
+			break;
+		case "queue":
+			if (checkIfQueue(SERVERQUEUE, message.channel)) queueList(message, SERVERQUEUE);
+			break;
+		default:
+			message.reply("nie wie jak korzystać z bota");
+			break;
 	}
-	if (ARGS[1] == "stacje") return list_stations(message.channel);
-	if (ARGS[1] == "pomocy")
-		return message.channel.send(
-			"```Dostępne polecenia:\n@Radio odpal < nazwa stacji > / <link z yt> - dołącza do kanału i odtwarza wybrane radio/film z linka\n@Radip queue - pokazuję aktualną kolejke \n@Radio idź - przestaje grać i wychodzi z kanału\n@Radio stacje - wyświetla dostępne stacje\n@Radio loop - zapętlanie utworów\n@Radio kloop - zapętla całą kolejkę \n@Radio pomiń - pomija element z kolejki```"
-		);
-	if (
-		checkIfOnChannel(message.member.voice.channel, message.channel) &&
-		checkIfOnSameVC(message.member.voice.channel, message.channel, SERVERQUEUE)
-	) {
-		if (ARGS[1] == "odpal") {
-			const PERMISSIONS = message.member.voice.channel.permissionsFor(CLIENT.user);
-			if (!PERMISSIONS.has("CONNECT") || !PERMISSIONS.has("SPEAK"))
-				return message.channel.send("No bym wbił ale nie moge 😕");
-			return execute(message, SERVERQUEUE, ARGS[2]);
-		}
-		if (ARGS[1] == "idź") {
-			message.channel.send("okok");
-			return stop_radio(SERVERQUEUE);
-		}
-		if (checkIfQueue(SERVERQUEUE, message.channel)) {
-			if (ARGS[1] == "pomiń") return skip(SERVERQUEUE);
-			if (ARGS[1] == "loop" || ARGS[1] == "kloop") return loop(message, SERVERQUEUE, ARGS[1]);
-			if (ARGS[1] == "queue") return queue(message, SERVERQUEUE);
-		} else return;
-	} else return;
-	return message.reply("nie wie jak korzystać z bota");
 });
-
-async function execute(message, queue, url) {
-	const MEDIAINFO = {
-		url: null,
-		name: null,
-		yt: false,
-	};
-
-	if (url.includes("youtu.be") || url.includes("youtube.com")) {
-		try {
-			const ytinfo = await ytdl.getInfo(url);
-			MEDIAINFO.url = url;
-			MEDIAINFO.name = ytinfo.videoDetails.title;
-			MEDIAINFO.yt = true;
-		} catch (err) {
-			logger.info(err);
-			return message.channel.send("Nie ma takiego filmu");
-		}
-	} else {
-		const STATIONNR = findStation(url);
-		if (STATIONNR == -1) return message.channel.send("O ch*j ci chodzi?");
-		const STATIONINFO = radiostation.stations[STATIONNR];
-		MEDIAINFO.url = STATIONINFO.url;
-		MEDIAINFO.name = STATIONINFO.desc;
-		MEDIAINFO.yt = false;
-	}
-
-	if (!queue) {
-		const MEDIACONSTRUCT = {
-			textChannel: message.channel,
-			voiceChannel: message.member.voice.channel,
-			connection: null,
-			media: [],
-			volume: 5,
-			lastName: null,
-			loop: false,
-			kloop: false,
-		};
-		QUEUE.set(message.guild.id, MEDIACONSTRUCT);
-		MEDIACONSTRUCT.media.push(MEDIAINFO);
-
-		try {
-			let connection = await message.member.voice.channel.join();
-			logger.info(`Polaczono z kanalem ${message.member.voice.channel.name}!`);
-			MEDIACONSTRUCT.connection = connection;
-			play(message.guild);
-			connection.on("disconnect", () => {
-				QUEUE.delete(message.guild.id);
-				logger.info(`Rozlaczono z kanalem ${message.member.voice.channel.name}!`);
-			});
-		} catch (err) {
-			logger.error(err);
-			QUEUE.delete(message.guild.id);
-			return;
-		}
-	} else {
-		queue.media.push(MEDIAINFO);
-		message.channel.send(`${MEDIAINFO.name} dodano do kolejki!`);
-	}
-}
-function checkIfOnChannel(voiceChannel, textChannel) {
-	if (!voiceChannel) {
-		textChannel.send("Najpierw wbij gdzieś!");
-		return false;
-	}
-	return true;
-}
 function checkIfQueue(queue, textChannel) {
 	if (!queue) {
 		textChannel.send("Brak kolejki!");
@@ -192,44 +143,132 @@ function checkIfQueue(queue, textChannel) {
 	}
 	return true;
 }
-function checkIfOnSameVC(voiceChannel, textChannel, queue) {
+function checkIfOnVC(voiceChannel, textChannel, queue) {
+	if (!voiceChannel) {
+		textChannel.send("Najpierw wbij gdzieś!");
+		return false;
+	}
 	if (queue && queue.voiceChannel != voiceChannel) {
 		textChannel.send("Przecież ciebie nawet tu nie ma");
 		return false;
 	}
 	return true;
 }
+
+async function execute(message, serverQueue, link) {
+	const voiceChannel = message.member.voice.channel;
+
+	if (!voiceChannel) return message.channel.send("Najpierw wbij gdzieś!");
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has("CONNECT") || !permissions.has("SPEAK"))
+		return message.channel.send("No bym wbił ale nie moge 😕");
+	if (serverQueue) {
+		if (serverQueue.voiceChannel != voiceChannel) {
+			return message.channel.send("Przecież ciebie nawet tu nie ma");
+		}
+	}
+	const mediaInfo = {
+		url: null,
+		name: null,
+		yt: null,
+	};
+
+	if (link.includes("youtube.com") || link.includes("youtu.be")) {
+		//sprawdz czy youtube
+		try {
+			const ytinfo = await ytdl.getInfo(link);
+			mediaInfo.url = link;
+			mediaInfo.name = ytinfo.videoDetails.title;
+			mediaInfo.yt = true;
+		} catch (err) {
+			logger.info(err);
+			return message.channel.send("Nie ma takiego filmu");
+		}
+	} else {
+		const stationNr = findStation(link);
+		if (stationNr == -1) {
+			return message.channel.send("O ch*j ci chodzi?");
+		}
+		const stationInfo = radiostation.stations[stationNr];
+		mediaInfo.url = stationInfo.url;
+		mediaInfo.name = stationInfo.desc;
+		mediaInfo.yt = false;
+	}
+
+	if (!serverQueue) {
+		const mediaConstruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			media: [],
+			volume: 5,
+			lastName: null,
+			loop: false,
+			kloop: false,
+		};
+		queue.set(message.guild.id, mediaConstruct);
+		mediaConstruct.media.push(mediaInfo);
+
+		try {
+			var connection = await voiceChannel.join();
+			logger.info(`Polaczono z kanalem ${voiceChannel.name}!`);
+			mediaConstruct.connection = connection;
+			play(message.guild);
+			connection.on("disconnect", () => {
+				queue.delete(message.guild.id);
+				logger.info(`Rozlaczono z kanalem ${voiceChannel.name}!`);
+			});
+		} catch (err) {
+			logger.error(err);
+			queue.delete(message.guild.id);
+			return;
+		}
+	} else {
+		serverQueue.media.push(mediaInfo);
+		message.channel.send(`${mediaInfo.name} dodano do kolejki!`);
+	}
+}
+
 function skip(queue) {
 	if (queue.loop) queue.media.shift();
 	queue.connection.dispatcher.end();
 }
+
 function play(guild) {
-	const SERVERQUEUE = QUEUE.get(guild.id);
-	if (!SERVERQUEUE.media[0]) return SERVERQUEUE.voiceChannel.leave();
-	let dispatcher;
-	if (SERVERQUEUE.media[0].yt) {
-		dispatcher = SERVERQUEUE.connection.play(
-			ytdl(SERVERQUEUE.media[0].url, { filter: "audioonly", highWaterMark: 1 << 25 })
+	const serverQueue = queue.get(guild.id);
+
+	if (!serverQueue.media[0]) {
+		serverQueue.voiceChannel.leave();
+		return;
+	}
+
+	var dispatcher;
+	if (serverQueue.media[0].yt) {
+		dispatcher = serverQueue.connection.play(
+			ytdl(serverQueue.media[0].url, { filter: "audioonly", highWaterMark: 1 << 25 })
 		);
-	} else dispatcher = SERVERQUEUE.connection.play(SERVERQUEUE.media[0].url);
+	} else {
+		dispatcher = serverQueue.connection.play(serverQueue.media[0].url);
+	}
 	dispatcher
 		.on("finish", () => {
-			if (SERVERQUEUE.kloop) SERVERQUEUE.media.push(SERVERQUEUE.media[0]);
-			if (!SERVERQUEUE.loop) SERVERQUEUE.media.shift();
+			if (serverQueue.kloop) serverQueue.media.push(serverQueue.media[0]);
+			if (!serverQueue.loop) serverQueue.media.shift();
 			play(guild);
 		})
 		.on("error", (err) => {
 			logger.error(err);
-			SERVERQUEUE.textChannel.send(`Coś się popierdoliło: ${err}`);
-			SERVERQUEUE.voiceChannel.leave();
+			serverQueue.textChannel.send(`Coś się popierdoliło: ${err}`);
+			serverQueue.voiceChannel.leave();
 		});
 
-	dispatcher.setVolumeLogarithmic(SERVERQUEUE.volume / 5);
-	if (SERVERQUEUE.lastName != SERVERQUEUE.media[0].name) {
-		SERVERQUEUE.textChannel.send(`Właśnie leci: **${SERVERQUEUE.media[0].name}**`);
-		SERVERQUEUE.lastName = SERVERQUEUE.media[0].name;
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+	if (serverQueue.lastName != serverQueue.media[0].name) {
+		serverQueue.textChannel.send(`Właśnie leci: **${serverQueue.media[0].name}**`);
 	}
+	serverQueue.lastName = serverQueue.media[0].name;
 }
+
 function loop(message, queue, loopMode) {
 	let text = "Powtarzanie ";
 	if (loopMode === "loop") {
@@ -245,29 +284,18 @@ function loop(message, queue, loopMode) {
 }
 
 function findStation(searchWord) {
-	let i = 0;
+	var i = 0;
 	while (radiostation.stations[i]) {
 		if (radiostation.stations[i].shortname === searchWord) return i;
 		i++;
 	}
 	return -1;
 }
+
 function stop_radio(queue) {
 	queue.media = [];
 	queue.connection.dispatcher.end();
 }
-function queue(message, queue) {
-	let text = "```";
-	text = text.concat(`Kolejka:\nWłaśnie leci:${queue.media[0].name}\n`);
-	for (let i = 1; i < queue.media.length; i++)
-		text = text.concat(`${i}. ${queue.media[i].name}\n`);
-	message.channel.send(text.concat("```"));
-}
-
-CLIENT.on("voiceStateUpdate", (oldMember) => {
-	const SERVERQUEUE = QUEUE.get(oldMember.guild.id);
-	if (SERVERQUEUE.voiceChannel.members.size === 1) stop_radio(SERVERQUEUE);
-});
 
 function list_stations(channel) {
 	let i = 0,
@@ -283,7 +311,16 @@ function refresh(channel) {
 	radiostation = JSON.parse(readFileSync("stations.json"));
 	channel.send("Odświeżam");
 }
+function queueList(message, queue) {
+	let text = "```";
+	text = text.concat(`Kolejka:\nWłaśnie leci:${queue.media[0].name}\n`);
+	for (let i = 1; i < queue.media.length; i++)
+		text = text.concat(i + " " + queue.media[i].name + "\n");
+	message.channel.send(text.concat("```"));
+}
 
-CLIENT.on("error", (error) => logger.error(error));
+client.on("error", (error) => {
+	logger.error(error);
+});
 
-CLIENT.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
